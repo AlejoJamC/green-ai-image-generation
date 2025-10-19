6be96f25ebd40b98c04925302d2eb4cb87c8bb75
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-10-19 23:53:35 +0200

    local test with Qwen/Qwen-Image-Edit

---
+++ qwen.py
@@ -0,0 +1,10 @@
+from diffusers import DiffusionPipeline
+from diffusers.utils import load_image
+
+pipe = DiffusionPipeline.from_pretrained("Qwen/Qwen-Image-Edit")
+
+prompt = "Turn this cat into a dog"
+input_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png")
+
+image = pipe(image=input_image, prompt=prompt).images[0]
+image.save("dog_image.png")
