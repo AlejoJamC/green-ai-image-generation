09842bb2a44f0ec53b3497644f228d6820a552c9
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-10-21 17:22:06 +0200

    image generated using sdxl by stable diffusion model diffusers/stable-diffusion-xl-1.0-inpainting-0.1

--- stable-diffusion.py
+++ stable-diffusion.py
@@ -1,31 +1,34 @@
 import torch
-from diffusers import StableDiffusionXLImg2ImgPipeline
+from diffusers import StableDiffusionXLInpaintPipeline
 from diffusers.utils import load_image
+from PIL import Image, ImageDraw
 
 device = "cpu"
 dtype = torch.float32
 
-print("Loading Stable Diffusion XL...")
-pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
+print("Loading SDXL Inpaint...")
+pipe = StableDiffusionXLInpaintPipeline.from_pretrained(
+    "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
     torch_dtype=dtype
 )
 pipe = pipe.to(device)
 
 input_image = load_image(
     "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png"
-)
+).resize((1024, 1024))
 
-print("Generating image...")
+mask = Image.new("L", (1024, 1024), 0)
+draw = ImageDraw.Draw(mask)
+draw.rectangle([300, 50, 700, 350], fill=255)
+
+print("Generating...")
 result = pipe(
-    prompt="a cat wearing a red hat on top of its head, detailed, photorealistic",
-    negative_prompt="no hat, hatless, without hat",
+    prompt="red hat",
     image=input_image,
-    strength=0.75,  # Greater strength = more alteration
-    num_inference_steps=30,
-    guidance_scale=7.5 # Higher guidance scale = closer to prompt
+    mask_image=mask,
+    num_inference_steps=25,
+    guidance_scale=8.0
 ).images[0]
-
 
 result.save("cat_with_hat.png")
 print("Image saved")
